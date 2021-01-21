b7161b8d53518b7c7552299ad5392343494c73eb
Ikea man <70744559+NLCheckmark@users.noreply.github.com>
2021-01-21 21:18:24 +0100

    Add files via upload

---
+++ SMITS.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/rederij Bestevaer_nav.css">
+    <link rel="stylesheet" href="css/rederij Bestevaer.css">
+</head>
+<body>
+<img src="https://stegro.nl/59-large_default/smits-shipping-.jpg" alt="Logo" width="200" height="200" style="float:right">
+    <ul class="nav">
+        <li><a class="nav" href="./rederij Bestevaer.html">Home</a></li>
+        <li><a href="#smits.php">SMITS</a></li>  
+        <li><a href="./rederij Bestevaer.php">bereken schip </a></li>   
+    </ul>
+
+    <h3>SMITS info</h3>
+    <div class="gallery">
+        <a target="_blank" href="https://www.ssrp.nl/uploads/ifader_content/5f3c143c-b367-4a8b-a607-49f1ded3d514/2951114872/0031-Bestevaer-P1050159.jpg">
+          <img src="https://www.ssrp.nl/uploads/ifader_content/5f3c143c-b367-4a8b-a607-49f1ded3d514/2951114872/0031-Bestevaer-P1050159.jpg" alt="rederij Bestevaer" width="300" height="250">
+        </a>
+        <button><div class="desc"><a href="https://www.marhisdata.nl/schip&id=16187">Ga naar SMITS info</a></div></button>
+      </div>
+  <div>
+</body>
+</html>
